bb985c63b790c2429d66b008b1876c517db0e5cf
melchiorn <m3lchior@gmail.com>
2014-05-02 19:27:18 +0200

    Add preseed for french localization + Remove grub boot timeout

--- src/poststrap.sh
+++ src/poststrap.sh
@@ -5,9 +5,21 @@ mkswap /swapfile1
 swapon /swapfile1
 echo "/swapfile1  none  swap  sw  0  0" >> /etc/fstab
 
+# Install VirtualBox  additions
 apt-get install -y linux-headers-$(uname -r)
 mount /dev/cdrom /media/cdrom
 sh /media/cdrom/VBoxLinuxAdditions.run
 
 mv /etc/rc.local.bak /etc/rc.local
+
+# display grub timeout and login promt after boot
+sed -i \
+  -e "s/quiet splash//" \
+  -e "s/GRUB_TIMEOUT=[0-9]/GRUB_TIMEOUT=0/" \
+  /etc/default/grub
+update-grub
+
+# clean up
+apt-get clean
+
 shutdown -h now
